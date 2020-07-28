e499d21b7bed3faf3936a5de1734c2df6a59ac43
Toan Nguyen <ngtranthanhtoan@gmail.com>
2020-07-28 11:43:44 +0700

    Updated

--- resources/views/template/show.blade.php
+++ resources/views/template/show.blade.php
@@ -26,15 +26,7 @@
 
 
 @section('footer')
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-<script>
-$(document).ready(function() {
-  $('#summernote').summernote({
-    height: 300
-  });
-});
-</script>
+
 @endsection
 
 @section('breadcrumb')
